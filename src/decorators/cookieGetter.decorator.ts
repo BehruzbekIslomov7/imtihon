f2d03c669382d8ff6be08e4a24f4d16b89a07774
BehruzbekIslomov7 <behruzbekislomov5@gmail.com>
2024-11-11 10:43:20 +0500

    yangilanish2

--- src/decorators/cookieGetter.decorator.ts
+++ src/decorators/cookieGetter.decorator.ts
@@ -2,16 +2,16 @@ import {
     ExecutionContext,
     UnauthorizedException,
     createParamDecorator,
-} from "@nestjs/common";
+  } from "@nestjs/common";
   
-export const CookieGetter = createParamDecorator(
-async (data: string, context: ExecutionContext) => {
-    const request = context.switchToHttp().getRequest();
-    const refresh_token = request.cookies[data];
-    if (!refresh_token) {
-    throw new UnauthorizedException("Unauthorizatd refresh token");
+  export const CookieGetter = createParamDecorator(
+    async (data: string, context: ExecutionContext) => {
+      const request = context.switchToHttp().getRequest();
+      const refresh_token = request.cookies[data];
+      if (!refresh_token) {
+        throw new UnauthorizedException("Unauthorizatd refresh token");
+      }
+      return refresh_token;
     }
-    return refresh_token;
-}
-);
+  );
   
